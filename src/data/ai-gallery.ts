9aaf396b4e516fa6b0eb1a6c1f8191fab43a896d
luyaowo <fly890611@163.com>
2026-03-13 17:40:42 +0800

    Aigc

--- src/data/ai-gallery.ts
+++ src/data/ai-gallery.ts
@@ -28,18 +28,53 @@ export const aiGallery: AIImage[] = [
   },
   {
     src: 'ai-02.jpg',
-    title: '机械花卉',
-    tool: 'DALL·E',
-    description: '机械与自然的边界探索',
+    title: '新艺术风格',
+    tool: 'Midjourney',
+    description: '花朵与自然有机风格',
     date: '2026-03-08',
-    prompt: 'A delicate flower made of brass and copper gears, with pistons and springs as petals, photorealistic, warm golden hour lighting, intricate mechanical details, steampunk style, soft focus background',
+    prompt: 'An elegant girl , surrounded by plants and flowers, pastel colors, flowers all over hair, Art Nouveau --v 5.2',
   },
   {
     src: 'ai-03.jpg',
-    title: '虚实之间',
-    tool: 'Stable Diffusion',
-    description: '超现实主义空间实验',
+    title: '花火',
+    tool: 'Midjourney',
+    description: '如果说你是海上的烟火',
     date: '2026-03-05',
-    prompt: 'An impossible architectural space where floating islands connect through cascading waterfalls, surrealism, dreamlike atmosphere, pastel color palette, floating geometric shapes, studio ghibli inspired, masterpiece',
+    prompt: 'https://s.mj.run/8fHk7ppQ2-U, A girl standing on the ground, holding small sparkler, cloud, soft light, lively brushwork, low poly, in the style of dreamlike illustrations, photoshop illustration --v 5.2',
   },
+  {
+    src: 'ai-04.jpg',
+    title: ' 猫',
+    tool: 'Midjourney',
+    description: '一只猫',
+    date: '2026-03-05',
+    prompt: 'light watercolor, a cute cat lying on a windowsill, bright, white background, few details, dreamy, studio Ghibli --v 5.1',
+  },
+{
+    src: 'ai-05.jpg',
+    title: '打坐的悟空',
+    tool: 'Midjourney',
+    description: '龙珠',
+    date: '2026-03-05',
+    prompt: 'Goku of little childhood in Dragon Ball, peacefully meditating in a traditional Chinese ink painting style, eyes closed, sitting cross-legged, with hands gracefully forming the Zen Mudra, front view, full body portrait, Zen, simple color scheme, Chinese Ink Painting, traditional Chinese minimalism, minimalist style, front view, full-body portrait, light and wet ink, high quality, 8k --ar 3:4 --niji 5',
+  },
+{
+    src: 'ai-06.jpg',
+    title: '猫猫侠',
+    tool: 'Midjourney',
+    description: '一个正义的猫猫侠',
+    date: '2026-03-05',
+    prompt: 'https://s.mj.run/E4tLsMoO7Y4 A cute cartoon character wearing a helmet with cat ears, two large eyes wearing a eye mask, confident expression, and wearing a emblem on the chest, wearing an aquamarine-colored cloak over its shoulders, blind box, Pop Mart, Pixar, clay material, 3d, c4d, OC render, RTX on, best --niji 6 --s 250 --ar 3:4',
+  },
+
+
+
+
+
+
+
+
+
+
+
 ];
